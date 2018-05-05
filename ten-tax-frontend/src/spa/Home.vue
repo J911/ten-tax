049cc1e092d593468db822d44a30766956fb1bba
Jay <fb.jaemin@gmail.com>
2018-05-05 23:47:52 +0900

    add AES

--- ten-tax-frontend/src/spa/Home.vue
+++ ten-tax-frontend/src/spa/Home.vue
@@ -1,12 +1,31 @@
 <template>
   <div id="home">
-    Home
+    <input type="password" placeholder="Password" v-model="password">
+    <textarea v-model="contents"></textarea>
+    {{ decryptContents }}
+    {{ encryptContents }}
   </div>
 </template>
 
 <script>
+import CryptoJS from "crypto-js";
+import hashData from "../assets/hashData.json"
 export default {
   name: 'Home',
+  data() {
+    return {
+      password: '',
+      contents: '',
+    }
+  },
+  computed: {
+    encryptContents() {
+      return CryptoJS.AES.encrypt(this.contents, this.password).toString()
+    },
+    decryptContents() {
+      return CryptoJS.AES.decrypt(hashData, this.password).toString(CryptoJS.enc.Utf8) || 'Not Matched Password'
+    }
+  }
 }
 </script>
 
